bf02eda0371b0c0a747cd3a58262afadd51c28d0
Jordan-Kennedy <35365534+Jordan-Kennedy@users.noreply.github.com>
2018-01-15 11:59:59 +0000

    Update and rename index.html to index.php
    Changed the doc type from .html to .php because this will allow the importing of php files. Changes have also been made to the documents structure to allow a greater variety of styling using CSS as well as improving the aesthetics.

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE php>
+<php>
+<head>
+	<title>Kyle's website</title>
+	<style>
+	</style>
+</head>
+  <body>
+    <h1>Welcome to Kyle Cottons Personal Website</h1>
+    <p>My html skills are very good.<p>
+  </body>
+</php>
