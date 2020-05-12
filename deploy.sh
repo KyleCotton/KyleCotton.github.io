2a28ce9e154d375e42ec451fb912c53d4abbd2f5
Kyle Cotton <kylecottonkc@gmail.com>
2020-05-12 17:59:47 +0100

    Updated Deploy Script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,35 @@
 git checkout source
+if [ $? -ne 0 ]
+then
+    echo "Failed to checkout source branch"; exit 1;
+fi
+
 hugo;
-cp -r public ./../
+if [ $? -ne 0 ]
+then
+    echo "Failed to  build site"; exit 1;
+fi
+
+cp -r public ~/workspace/
+if [ $? -ne 0 ]
+then
+    echo "Failed to copy public"; exit 1;
+fi
+
+git checkout master
+if [ $? -ne 0 ]
+then
+    echo "Failed to checkout master branch"; exit 1;
+fi
+
+rm -rf ~/workspace/KyleCotton.github.io/*
+if [ $? -ne 0 ]
+then
+    echo "Failed to remove old files"; exit 1;
+fi
+
+mv ~/workspace/public/* ~/workspace/KyleCotton.github.io/
+if [ $? -ne 0 ]
+then
+    echo "Failed to copy new files"; exit 1;
+fi
